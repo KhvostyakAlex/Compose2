d15f026a4917bfa0b420cb33fc4e5e781f130b3b
Khvostyak <73226448+KhvostyakAlex@users.noreply.github.com>
2021-08-10 16:19:16 +0700

    first commit

--- app/src/main/java/ru/leroymerlin/internal/compose2/MainActivity.kt
+++ app/src/main/java/ru/leroymerlin/internal/compose2/MainActivity.kt
@@ -3,11 +3,16 @@ package ru.leroymerlin.internal.compose2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.leroymerlin.internal.compose2.ui.theme.Compose2Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,8 +22,7 @@ class MainActivity : ComponentActivity() {
             Compose2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("WOrld!!!")
-                    Greeting("Android!!!")
+                    Greeting("WOrld!!!", "Android")
                 }
             }
         }
@@ -26,8 +30,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, lastName: String) {
+    Column{
+        Text(text = "Hello $name!")
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "Hello $lastName!")
+    }
+
 
 }
 
@@ -35,7 +44,7 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     Compose2Theme {
-        Greeting("Android")
-        Greeting("Android")
+        Greeting("Android", "Android")
+
     }
 }
