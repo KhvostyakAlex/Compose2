51522f3885035a8d557a73b2699ed3a1d1fb61ad
Khvostyak <73226448+KhvostyakAlex@users.noreply.github.com>
2021-08-18 16:03:16 +0700

    addCard

--- app/src/main/java/ru/leroymerlin/internal/compose2/ui/screens/ListScreen.kt
+++ app/src/main/java/ru/leroymerlin/internal/compose2/ui/screens/ListScreen.kt
@@ -1,16 +1,24 @@
 package ru.leroymerlin.internal.compose2.ui.screens
 
+
+
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontSynthesis.Companion.All
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.unit.dp
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
+
 import androidx.navigation.compose.rememberNavController
 
 data class Word (val value: String)
@@ -24,15 +32,31 @@ fun ListScreen(navController: NavController){
         LazyColumn(){
             testArray.map {
                 item {
-                    Text(it, modifier = Modifier
+                        Card(
+                            elevation = 8.dp,
+                            backgroundColor = Color.White,
+                            modifier = Modifier.fillMaxWidth().padding(4.dp)
+                        ) {
+                            Text(it, modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxWidth()
+                                .clickable {
+                                    navController.navigate("details")
+                                })
+                        }
+
+
+                /*Text(it, modifier = Modifier
                         .padding(24.dp)
                         .fillMaxWidth()
                         .clickable {
-                          navController.navigate("details")
-                        })
+                            navController.navigate("details")
+                        })*/
                 }
             }
         }
 
     }
 }
+
+
--- app/src/main/java/ru/leroymerlin/internal/compose2/ui/screens/SearchScreen.kt
+++ app/src/main/java/ru/leroymerlin/internal/compose2/ui/screens/SearchScreen.kt
@@ -1,17 +1,61 @@
 package ru.leroymerlin.internal.compose2.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontSynthesis.Companion.All
 import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun SearchScreen(){
-    Scaffold() {
-        Text("Search Screen", modifier = Modifier.padding(24.dp))
+    Scaffold {
+     /*   topBar = {
+            TopAppBar{
+                Text(text = "text")
+            }
+        }*/
+            val textState = remember { mutableStateOf("")}
+            val isEnabled = remember { mutableStateOf(false)}
+            val isCompleteLogin = remember { mutableStateOf(0)}
+
+
+
+        Column {
+            Text("Search Screen", modifier = Modifier.padding(24.dp))
+
+            Row {
+               // Text("Строка поиска", modifier = Modifier.padding(24.dp))
+                TextField(value = textState.value,
+                    onValueChange = { value -> textState.value = value},
+                    placeholder = {Text("Ввведи Фамилию/LDAP")},
+                    trailingIcon = { if(textState.value.length ==8){ Text("V") } } //при вводе 8 знаков появится иконка
+
+                )
+
+
+
+                Button(onClick = {
+                    isEnabled.value = isEnabled.value==false
+
+                },colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)) {
+                    Text("Поиск", color = Color.White)
+                }
+
+
+        }
+            if(isEnabled.value){
+                Text("что-то ищем", modifier = Modifier.padding(24.dp))
+            }
+
+
+        }
+
     }
 }
